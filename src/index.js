3881b41d4fcc210f4412c4379554227e8d5b47b0
Belen Neira <belen.neira05@gmail.com>
2023-02-14 20:35:57 -0300

    14/02 Primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import validator from './validator.js';
+const input = document.getElementById('credit-card-input');
+const button = document.getElementById('validate-button');
+const output = document.getElementById('output');
 
+button.addEventListener('click', function() {
+  if (validator.isValid(input.value)) {
+    output.innerHTML = `Tarjeta de crédito válida: ${validator.maskify(input.value)}`;
+  } else {
+    output.innerHTML = 'Tarjeta de crédito inválida';
+  }
+});
 console.log(validator);
