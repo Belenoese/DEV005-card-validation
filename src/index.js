029637379f45172f2feb6b5480f8ae9b140362ff
Belen Neira <belen.neira05@gmail.com>
2023-03-02 13:35:04 -0300

    Se implementa funcionalidad y diseño de la pagina

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
-import validator from './validator.js';
-const input = document.getElementById('credit-card-input');
-const button = document.getElementById('validate-button');
-const output = document.getElementById('output');
+import validator from "./validator.js";
+const validar = document.getElementById("validar-tarjeta");
 
-button.addEventListener('click', function() {
-  if (validator.isValid(input.value)) {
-    output.innerHTML = `Tarjeta de crédito válida: ${validator.maskify(input.value)}`;
+//se llama a la funcion del validator.js
+validar.addEventListener("click", (event) => {
+  const numeroDeTarjeta = document.getElementById("numero-tarjeta");
+  const fechaVencimieno = document.getElementById("fecha-vencimiento");
+  const codigoVerificacion = document.getElementById("codigo-verificacion");
+
+  if (
+    numeroDeTarjeta.value === "" ||
+    fechaVencimieno.value === "" ||
+    codigoVerificacion.value === ""
+  ) {
+    event.preventDefault();
+    alert("Por favor complete todos los campos!");
   } else {
-    output.innerHTML = 'Tarjeta de crédito inválida';
+    const resultado = validator.isValid(numeroDeTarjeta.value);
+    //console.log (resultado)
+    if (resultado === true) {
+      document.getElementById("resultado").innerHTML = "Tarjeta Válida";
+    } else {
+      document.getElementById("resultado").innerHTML = "Tarjeta Inválida";
+    }
+
+    console.log(resultado);
+
+    document.getElementById("informacionDeTarjeta").style.display = "none";
+    document.getElementById("resultado").style.display = "block";
+    document.getElementById("ocultarNumeros").textContent =
+      validator.maskify(numeroDeTarjeta.value);
   }
 });
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,42 @@
 const validator = {
-  //...
+  //aplicando algoritmo de luhn
+  isValid: (numero) => {
+    //obteniendo la reversa del numero a traves de un array
+    const array = numero.split("").reverse();
+    //console.log(array);
+    //se declaran las variable de los valores que necesitamos obtener
+    let impares = 0;
+    let paresMultiplicados = 0;
+    let suma2Digitos = 0;
+
+    //tomando los numeros en las posiciones impares
+    for (let i = 0; i < array.length; i++) {
+      if (i % 2 !== 1) {
+        //si la posicion es impar
+        impares += parseInt(array[i]);
+      } else {
+        //si la posicion es par
+        const multiplicacion = array[i] * 2;
+        if (multiplicacion >= 10) {
+          const digito1 = parseInt(multiplicacion.toString().charAt(0));
+          const digito2 = parseInt(multiplicacion.toString().charAt(1));
+          suma2Digitos += digito1 + digito2;
+        } else {
+          paresMultiplicados += multiplicacion;
+        }
+      }
+    }
+
+    const sumaTotal = impares + paresMultiplicados + suma2Digitos;
+
+    return (sumaTotal % 10 === 0)
+  },
   
+  maskify: (numero) => {
+    const mascara = numero.slice(0, -4).replace(/./g, "#") + numero.slice(-4);
+    console.log(mascara);
+    return mascara;
+  },
 };
 
 export default validator;
